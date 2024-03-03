638e0bc03febb467a9584482181fe6b0c7663fae
Влад Сологуб <143345251+vladsologyb@users.noreply.github.com>
2024-03-03 22:35:53 +0200

    hw-6

--- js/task-2.js
+++ js/task-2.js
@@ -1,40 +1,31 @@
 'use strict';
 
-function calcAverageCalories(days) {
-     if (days.length === 0) {
-            return 0
-        }
-    let totalCalories = 0;
-    for (const day of days) {
-       totalCalories += day.calories 
+class Storage {
+    #items;
+
+    constructor(items) {
+        this.#items = items;
     }
-    return totalCalories / days.length
+
+    getItems() {
+        return this.#items;
+    }
+
+    addItem(newItem) {
+        this.#items.push(newItem);
+    }
+
+    removeItem(itemToRemove) {
+        const itemIndex = this.#items.indexOf(itemToRemove);
+        if (itemIndex !== -1) {
+            this.#items.splice(itemIndex, 1);
+        }
+    }
 }
 
-console.log(
-  calcAverageCalories([
-    { day: "monday", calories: 3010 },
-    { day: "tuesday", calories: 3200 },
-    { day: "wednesday", calories: 3120 },
-    { day: "thursday", calories: 2900 },
-    { day: "friday", calories: 3450 },
-    { day: "saturday", calories: 3280 },
-    { day: "sunday", calories: 3300 }
-  ])
-); // 3180
-
-console.log(
-  calcAverageCalories([
-    { day: "monday", calories: 2040 },
-    { day: "tuesday", calories: 2270 },
-    { day: "wednesday", calories: 2420 },
-    { day: "thursday", calories: 1900 },
-    { day: "friday", calories: 2370 },
-    { day: "saturday", calories: 2280 },
-    { day: "sunday", calories: 2610 }
-  ])
-); // 2270
-
-console.log(
-  calcAverageCalories([])
-); // 0
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
